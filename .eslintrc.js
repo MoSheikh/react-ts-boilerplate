d0693b99a77d4f026afc4a61289a6989463ca0be
Mohammad Imran Sheikh <msheik24@uwo.ca>
2021-06-23 23:12:36 -0400

    Add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,37 @@
 module.exports = {
-	root: true,
-	env: {
-		node: true,
-	},
-	parser: "@typescript-eslint/parser",
-	plugins: [
-		'@typescript-eslint'
-	],
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended'
-	]
-}
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'jest',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  extends: [
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  rules: {
+    '@typescript-eslint/semi': 'off',
+    'no-prototype-builtins': 'off',
+
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+      },
+    },
+  ],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -9,81 +9,81 @@ const SRC_DIR = path.resolve(__dirname, 'src')
 const OUT_DIR = path.resolve(__dirname, 'build')
 
 module.exports = {
-	entry: path.resolve(SRC_DIR, 'index.tsx'),
+  entry: path.resolve(SRC_DIR, 'index.tsx'),
 
-	output: {
-		path: OUT_DIR,
-		filename: 'static/js/[name].[contenthash:8].js',
-		chunkFilename: 'static/js/[name].[contenthash:8].chunk.js',
-		publicPath: '/',
-	},
+  output: {
+    path: OUT_DIR,
+    filename: 'static/js/[name].[contenthash:8].js',
+    chunkFilename: 'static/js/[name].[contenthash:8].chunk.js',
+    publicPath: '/',
+  },
 
-	devServer: {
-		open: true,
-		host: process.env.HOST || 'localhost',
-		port: parseInt(process.env.PORT || '8000', 10),
-		historyApiFallback: true
-	},
+  devServer: {
+    open: true,
+    host: process.env.HOST || 'localhost',
+    port: parseInt(process.env.PORT || '8000', 10),
+    historyApiFallback: true,
+  },
 
-	module: {
-		rules: [
-			{
-				use: [
-					isProduction
-						? MiniCssExtractPlugin.loader
-						: 'style-loader',
-					'css-loader'
-				],
-				test: /\.css/i,
-				include: /src/,
-			},
-			{
-				loader: 'babel-loader',
-				test: /\.(js|jsx|ts|tsx)$/i,
-				exclude: /node_modules/,
-			},
-			{
-				loader: 'file-loader',
-				test: /\.(jpe?g|png|svg)$/i,
-				options: {
-					name: 'static/media/[name][hash:8].[ext]'
-				}
-			},
-			{
-				loader: 'file-loader',
-				test: /\.(woff|woff2|eot|ttf|otf)$/i,
-				options: {
-					name: 'static/fonts/[name][hash:8].[ext]'
-				}
-			}
-		],
-	},
+  module: {
+    rules: [
+      {
+        use: [
+          isProduction
+            ? MiniCssExtractPlugin.loader
+            : 'style-loader',
+          'css-loader',
+        ],
+        test: /\.css/i,
+        include: /src/,
+      },
+      {
+        loader: 'babel-loader',
+        test: /\.(js|jsx|ts|tsx)$/i,
+        exclude: /node_modules/,
+      },
+      {
+        loader: 'file-loader',
+        test: /\.(jpe?g|png|svg)$/i,
+        options: {
+          name: 'static/media/[name][hash:8].[ext]',
+        },
+      },
+      {
+        loader: 'file-loader',
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        options: {
+          name: 'static/fonts/[name][hash:8].[ext]',
+        },
+      },
+    ],
+  },
 
-	resolve: {
-		extensions: ['*', '...', '.jsx', '.tsx'],
-	},
+  resolve: {
+    extensions: ['*', '...', '.jsx', '.tsx'],
+  },
 
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: process.env.APP_TITLE || 'Untitled',
-			inject: true,
-			template: path.resolve(SRC_DIR, 'index.html'),
-			favicon: path.resolve(SRC_DIR, 'assets', 'icons', 'react.png'),
-			... isProduction && {
-				minify: {
-					collapseWhitespace: true,
-					keepClosingSlash: true,
-					removeComments: true,
-					removeRedundantAttributes: true,
-					removeScriptTypeAttributes: true,
-					removeStyleLinkTypeAttributes: true,
-					removeEmptyAttributes: true,
-					useShortDoctype: true,
-					minifyCSS: true,
-					minifyJS: true,
-					minifyURSLs: true
-				}
-			},
-		}),
-	],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: process.env.APP_TITLE || 'Untitled',
+      inject: true,
+      template: path.resolve(SRC_DIR, 'index.html'),
+      favicon: path.resolve(SRC_DIR, 'assets', 'icons', 'react.png'),
+      ...isProduction && {
+        minify: {
+          collapseWhitespace: true,
+          keepClosingSlash: true,
+          removeComments: true,
+          removeRedundantAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          removeEmptyAttributes: true,
+          useShortDoctype: true,
+          minifyCSS: true,
+          minifyJS: true,
+          minifyURSLs: true,
+        },
+      },
+    }),
+  ],
 }
