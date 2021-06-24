9851ff27db6c2bf2ee02d511232d0daa125510bf
Mohammad Sheikh <msheik24@uwo.ca>
2021-06-24 17:00:53 -0400

    Fix jest integration

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+  preset: "ts-jest/presets/default-esm",
+  extensionsToTreatAsEsm: [".ts"],
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    ".(css|less)$": "identity-obj-proxy",
+  },
   globals: {
-    "ts-jest": {},
+    "ts-jest": {
+      useESM: true,
+      tsconfig: "tsconfig.json",
+    },
   },
 };
--- src/util/math.test.ts
+++ src/util/math.test.ts
@@ -1,3 +1,3 @@
-import { sum } from "./index";
+import { sum } from "./math";
 
 test("adds 1 + 2 expecting 3", () => expect(sum(1, 2)).toBe(3));
